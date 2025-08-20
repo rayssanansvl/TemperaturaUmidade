786a0cb1869fa7351e0d0c4e4c7d6baf031599e4
rayssanansvl <rayssa.senaiii@gmail.com>
2025-08-20 11:46:57 -0300

    Alteração no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   // Configurações do MQTT
   const brokerHost = "broker.hivemq.com";
   const brokerPort = 8884;
-  const topic = "rayssa/temperatura";
+  const topic = "rayssa/lerSensor";
 
   // Hook customizado para gerenciar MQTT
   const { temperature, humidity, isConnected, lastUpdate, connectionAttempts } =
